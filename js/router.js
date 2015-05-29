d346b3d58536f6762fb232c68f765f5c6c27f94c
kubota <kkubota@jlce.jp>
2015-05-29 17:02:25 +0900

    add noteFormView.

--- js/router.js
+++ js/router.js
@@ -3,6 +3,7 @@
 App.Router = Backbone.Router.extend({
 	routes: {
 		'notes/:id': 'showNoteDetail',
+		'new': 'showNewNote',
 		'*actions': 'defaultRoute'
 	},
 
@@ -24,11 +25,11 @@ App.Router = Backbone.Router.extend({
 		// render()を実行してDOM要素を自身のelに挿入する
 		App.mainContainer.show(noteListView);
 
-		// [New Note]ボタン
+		// [New Note]ボタンと検索窓
 		this.showNoteControl();
 	},
 
-	// [New Note]ボタン
+	// [New Note]ボタンと検索窓
 	showNoteControl: function() {
 		var noteControlView = new App.NoteControlView();
 		App.headerContainer.show(noteControlView);
@@ -44,6 +45,35 @@ App.Router = Backbone.Router.extend({
 		App.mainContainer.show(noteDetailView);
 
 		//[New Note]ボタンは消したいのでビュー破棄しておく
+		App.headerContainer.empty();
+	},
+
+	showNewNote: function() {
+
+		var self = this;
+
+		// テンプレートの<%= title %>などの出力を空文字列で
+		// 空欄にしておくため、新規に生成したNoteモデルを渡して
+		// NoteFormViewを初期化する
+		var noteFormView = new App.NoteFormView({
+			model: new App.Note()
+		});
+
+		noteFormView.on('submit:form', function(attrs) {
+			// submit:formイベントで受け取ったフォームの
+			// 入力値をNoteCollectionコレクションのcreate()に
+			// 渡してNoteモデルの新規作成と保存を行う
+			App.noteCollection.create(attrs);
+
+			// モデル一覧を表示してルートを#notesに戻す
+			self.showNoteList();
+			self.navigate('notes');
+		});
+
+		App.mainContainer.show(noteFormView);
+
+		// [New Note]ボタンはこの画面では必要ないので
+		// ビューを破棄しておく
 		App.headerContainer.empty();
 	}
 });
